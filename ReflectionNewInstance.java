8b3746ae90562facc8030acc720458b8156bb943
jwang1 <sigkdd@yahoo.com>
2019-01-13 00:03:06 -0500

    Using Class newInstance to dyanmically instantiate objects

---
+++ ReflectionNewInstance.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.*;
+
+public class ReflectionNewInstance {
+
+	public static void main(String[] argvs) throws Exception  {
+
+		Class<String> cz = (Class<String>) Class.forName("java.lang.String");
+
+    String s = cz.newInstance();
+
+		Field fv = s.getClass().getDeclaredField("value");
+		fv.setAccessible(true);
+		Field mf = Field.class.getDeclaredField("modifiers");
+		mf.setAccessible(true);
+		mf.setInt(fv, fv.getModifiers() & ~Modifier.FINAL);
+
+		fv.set(s, "created from Class newInstance() and with final field value updates".getBytes()); 
+
+		mf.setInt(fv, fv.getModifiers() | Modifier.FINAL);
+		mf.setAccessible(false);
+
+		fv.setAccessible(false);
+
+    System.out.println("^^^^^^^^^^^ newInstance() created : " + s);
+	}
+
+}
