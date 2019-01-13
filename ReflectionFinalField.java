082253bde89928a0630f393afcc51495cf19cb37
jwang1 <sigkdd@yahoo.com>
2019-01-12 23:14:00 -0500

    Created standalone class to test updating final field

---
+++ ReflectionFinalField.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ReflectionFinalField { 
+
+	public static void main(String[] argvs) {
+
+		String s = new String("before changes");
+
+		Class<?> cs = s.getClass();
+
+		List<Field> flds = Arrays.asList(cs.getDeclaredFields());
+
+		flds.forEach(f -> {
+			try {
+				f.setAccessible(true);
+
+				Field mf = Field.class.getDeclaredField("modifiers");
+				mf.setAccessible(true);
+				// using Reflection Modifier Field to remove String object's final field value/coder/hash
+				mf.setInt(f, f.getModifiers() & ~Modifier.FINAL);
+				System.out.println(f + " _^:^_ " + f.get(s));
+
+				// Now update the value, and check coder and hash
+				if (f.getName().equals("value")) {
+					System.out.println("####################### prior to changes; string --> " + s);
+					// f.set(s, "*!after changes!*".getBytes());
+					f.set(s, "*!after changes!*");
+					System.out.println("####################### after changes; string --> " + s);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+
+		// Change the string
+		try {
+			Field fv = cs.getDeclaredField("value");
+			fv.setAccessible(true);
+			Field mf = Field.class.getDeclaredField("modifiers");
+			mf.setAccessible(true);
+			mf.setInt(fv, fv.getModifiers() & ~Modifier.FINAL);
+
+			fv.set(s, "_After changes_".getBytes());
+
+			System.out.println("#### after changes,  now the string is : " + s);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+	}
+}
+
+
