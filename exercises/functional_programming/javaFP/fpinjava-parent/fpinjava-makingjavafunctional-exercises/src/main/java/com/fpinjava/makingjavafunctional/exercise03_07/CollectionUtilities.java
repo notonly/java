8b45251fcc22b1f86d561c48b2045ad1766bac30
jwang1 <sigkdd@yahoo.com>
2019-02-20 20:02:02 -0500

    FP in Java
    doing Exercise 3-7 : foldRight
    
    almost worked long way to traverse the list backward

--- exercises/functional_programming/javaFP/fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_07/CollectionUtilities.java
+++ exercises/functional_programming/javaFP/fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_07/CollectionUtilities.java
@@ -59,7 +59,13 @@ public class CollectionUtilities {
   public static <T, U> U foldRight(List<T> ts,
                                    U identity,
                                    Function<T, Function<U, U>> f) {
-    throw new RuntimeException("To be implemented");
+    U rslt = identity;
+
+    for (int i = ts.size() - 1; i >=0; i--) {
+      rslt = f.apply(ts.get(i)).apply(rslt);
+    }
+
+    return rslt;
   }
 
   public static <T> List<T> append(List<T> list, T t) {
