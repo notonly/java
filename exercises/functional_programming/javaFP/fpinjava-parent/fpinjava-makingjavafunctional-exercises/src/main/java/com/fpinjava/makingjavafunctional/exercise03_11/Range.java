975d2653993dd84d4857522f6d0d364a090e8715
jwang1 <sigkdd@yahoo.com>
2019-03-18 23:44:38 -0400

    add some Python notes
    some books for beginers

--- exercises/functional_programming/javaFP/fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
+++ exercises/functional_programming/javaFP/fpinjava-parent/fpinjava-makingjavafunctional-exercises/src/main/java/com/fpinjava/makingjavafunctional/exercise03_11/Range.java
@@ -1,11 +1,27 @@
 package com.fpinjava.makingjavafunctional.exercise03_11;
 
+import com.fpinjava.makingjavafunctional.exercise03_10.CollectionUtilities;
+
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Range {
 
   public static List<Integer> range(int start, int end) {
-    throw new RuntimeException("To be implemented");
+    if (start > end) {
+      return CollectionUtilities.list();
+    }
+
+    List<Integer> result = new ArrayList<>(end - start + 1);
+
+    int idx = start;
+    while (idx < end) {
+      CollectionUtilities.append(result, idx);
+
+      idx++;
+    }
+
+    return result;
   }
 }
